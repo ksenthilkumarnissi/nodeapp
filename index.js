64180706bc5c469bd6ebf27058d4652db2036aed
Senthil Kumar <senthilk@nissisoftwaresystems.com>
2022-03-01 11:49:28 +0530

    port change

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/nodeapp', (req, res) => {
+  res.send("Node App Running");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
