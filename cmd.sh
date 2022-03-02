7567cca6441391628d7d544fcc04a2da2a87c44b
Senthil Kumar <senthilk@nissisoftwaresystems.com>
2022-03-02 18:46:34 +0530

    sleep 20

--- cmd.sh
+++ cmd.sh
@@ -1,19 +1,36 @@
 echo "-----------------Date-----------------"
 date
+sleep 20
+
 echo "-----------------Node Version-----------------"
 node -v
+sleep 20
+
 echo "-----------------List Files-----------------"
 ls -lrt
+sleep 20
+
 echo "-----------------pwd-----------------"
 pwd
+sleep 20
+
 echo "-----------------Ifconfig-----------------"
 ifconfig
+sleep 20
+
 echo "-----------------Netstat-----------------"
 netstat -at -np
+sleep 20
+
 echo "-----------------Docker-----------------"
 docker ps
+sleep 20
+
 echo "-----------------systemctl-----------------"
 systemctl status docker
+sleep 20
+
 echo "-----------------Curl-----------------"
 curl -i http://127.0.0.1:4000/
+sleep 20
 return 0
