eafc38c0dcabb584e6b1481488eebbe99020877e
Senthil Kumar <senthilk@nissisoftwaresystems.com>
2022-03-02 18:36:57 +0530

    echo "-----------------Date-----------------"

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,12 @@
+echo "-----------------Date-----------------"
 date
+echo "-----------------Ifconfig-----------------"
 ifconfig
+echo "-----------------Netstat-----------------"
 netstat -at -np
+echo "-----------------Docker-----------------"
 docker ps
+echo "-----------------systemctl-----------------"
 systemctl status docker
+echo "-----------------Curl-----------------"
 curl -i http://127.0.0.1:4000/
